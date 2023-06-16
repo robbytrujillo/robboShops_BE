0eb48c657224b781615474faf0359c40068fd5d3
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-06-16 11:15:48 +0700

    robboshops BE | Table Name Automatitation

---
+++ application/core/MY_Model.php
@@ -0,0 +1,20 @@
+<?php 
+
+defined('BASEPATH') OR exit ('No direct script access allowed');
+
+class MY_Model extends CI_Model {
+    protected $table = '';
+
+    public function __construct() {
+        parent:: __construct();
+
+        if (!$this->table) {
+            $this->table = strtolower(
+                str_replace('_model', '', get_class($this))
+            );
+        }
+    }
+}
+
+
+?>
